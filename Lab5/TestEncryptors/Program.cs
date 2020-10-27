f8476aa9213a297332464a042e8a8d501128807b
FranciscoJMorales <55027785+FranciscoJMorales@users.noreply.github.com>
2020-10-27 17:54:44 -0600

    Creación de los proyectos

---
+++ Lab5/TestEncryptors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestEncryptors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lab5/api/Controllers/EncryptorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncryptorController : ControllerBase
+    {
+        readonly IWebHostEnvironment env;
+
+        public EncryptorController(IWebHostEnvironment _env)
+        {
+            env = _env;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [Route("/api/cipher/{method}")]
+        public IActionResult Cipher([FromForm] IFormFile file, [FromForm] string key, string method)
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("/api/decipher")]
+        public IActionResult Decipher([FromForm] IFormFile file, [FromForm] string key)
+        {
+            return Ok();
+        }
+    }
+}
